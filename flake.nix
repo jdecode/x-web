5e69184e370c94bdbac2e8c294bce7aa3930907b
Jagdeep Singh Kalsi <jdecode@gmail.com>
2025-05-11 17:14:06 +0530

    Add flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -1,21 +1,28 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  description = "Laravel Dev Environment";
 
-let
-  php = pkgs.php84;
-in
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
 
-pkgs.mkShell {
-  buildInputs = [
-    php
-    pkgs.php84Packages.composer
-    pkgs.mariadb
-    pkgs.postgresql
-    pkgs.nodejs_22
-    pkgs.openssl
-  ];
+  outputs = { nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.php84
+        pkgs.php84Packages.composer
+        pkgs.postgresql
+        pkgs.mariadb
+        pkgs.nodejs_22
+        pkgs.openssl
+      ];
 
-  shellHook = ''
-    export PATH=$PATH:./vendor/bin
-  '';
+      shellHook = ''
+        export PATH=$PATH:./vendor/bin
+        echo "✅ Laravel dev shell ready (PHP 8.4, Composer, Node 22)"
+      '';
+    };
+  };
 }
-
